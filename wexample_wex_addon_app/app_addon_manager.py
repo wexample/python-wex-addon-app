cc9b6779f04570d63148a3d1d1abe32303ab8b3e
weeger <romain.weeger@gmail.com>
2025-08-16 17:47:47 +0200

    Store call workdir

--- wexample_wex_addon_app/app_addon_manager.py
+++ wexample_wex_addon_app/app_addon_manager.py
@@ -1,6 +1,5 @@
+from functools import cached_property
 from typing import TYPE_CHECKING
-
-from pydantic import PrivateAttr
 
 from wexample_wex_core.common.abstract_addon_manager import AbstractAddonManager
 
@@ -9,14 +8,10 @@ if TYPE_CHECKING:
 
 
 class AppAddonManager(AbstractAddonManager):
-    _app_workdir: "Workdir" = PrivateAttr()
-
-    def get_workdir(self) -> "Workdir":
-        if not self._app_workdir:
-            from wexample_wex_core.workdir.workdir import Workdir
-            self._app_workdir = Workdir(
-                io=self.kernel.io,
-                path=self.kernel.call_workdir.get_path()
-            )
-
-        return self._app_workdir
+    @cached_property
+    def app_workdir(self) -> "Workdir":
+        from wexample_wex_core.workdir.workdir import Workdir
+        return Workdir(
+            io=self.kernel.io,
+            path=self.kernel.call_workdir.get_path()
+        )
