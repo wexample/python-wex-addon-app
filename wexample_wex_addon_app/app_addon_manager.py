bab4b682fd9e9e943e7b14fcc631435b34750f7b
weeger <romain.weeger@gmail.com>
2025-08-20 21:56:08 +0200

    Load config

--- wexample_wex_addon_app/app_addon_manager.py
+++ wexample_wex_addon_app/app_addon_manager.py
@@ -12,7 +12,15 @@ class AppAddonManager(AbstractAddonManager):
     def app_workdir(self) -> "ProjectWorkdir":
         from wexample_wex_core.workdir.project_workdir import ProjectWorkdir
 
-        return ProjectWorkdir.create_from_path(
-            path=self.kernel.call_workdir.get_path(),
+        path = self.kernel.call_workdir.get_path()
+        workdir = ProjectWorkdir.create_from_path(
+            path=path,
             io=self.kernel.io,
         )
+
+        from wexample_wex_addon_app.const.globals import APP_FILE_APP_CONFIG
+        config_yml = workdir.find_by_name_recursive(APP_FILE_APP_CONFIG)
+        if config_yml is not None:
+            manager_config = config_yml.read_as_config().search('files_state.manager')
+            file_relative = manager_config.get_config_item('file')
+            class_name = manager_config.get_config_item('class')
