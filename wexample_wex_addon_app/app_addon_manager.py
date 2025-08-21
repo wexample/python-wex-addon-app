752102a30e18519de677c50641ad4c01a20d1b4c
weeger <romain.weeger@gmail.com>
2025-08-21 16:04:36 +0200

    Project level class

--- wexample_wex_addon_app/app_addon_manager.py
+++ wexample_wex_addon_app/app_addon_manager.py
@@ -10,9 +10,7 @@ if TYPE_CHECKING:
 class AppAddonManager(AbstractAddonManager):
     @cached_property
     def app_workdir(self) -> "ProjectWorkdir":
-        from wexample_wex_addon_app.const.globals import APP_FILE_APP_CONFIG
         from wexample_wex_core.workdir.project_workdir import ProjectWorkdir
-        from wexample_helpers.helpers.module import module_load_class_from_file
 
         path = self.kernel.call_workdir.get_path()
         # Use basic project class to access minimal configuration.
@@ -21,37 +19,12 @@ class AppAddonManager(AbstractAddonManager):
             io=self.kernel.io,
         )
 
-        manager_config = workdir.get_config().search('files_state.manager')
-        if manager_config:
-            file_relative = manager_config.get_config_item('file').get_str()
-            class_name = manager_config.get_config_item('class').get_str()
+        preferred = workdir.get_preferred_workdir_class()
+        if preferred:
+            return preferred.create_from_path(
+                path=path,
+                io=self.kernel.io,
+            )
 
-            # Compute absolute path to the python file
-            file_abs_path = (path / file_relative)
-
-            if file_abs_path.exists():
-                # Dynamically load the module and fetch the class
-                class_module = module_load_class_from_file(
-                    file_path=file_abs_path,
-                    class_name=class_name,
-                )
-
-                # Good format
-                if issubclass(class_module, ProjectWorkdir):
-                    # Replace the basic workdir
-                    return class_module.create_from_path(
-                        path=path,
-                        io=self.kernel.io,
-                    )
-                else:
-                    workdir.io.warning(
-                        f"Custom class '{class_name}' defined in {APP_FILE_APP_CONFIG} was found at {file_abs_path}, "
-                        f"but it must be a subclass of {ProjectWorkdir.__name__}."
-                    )
-            else:
-                workdir.io.warning(
-                    f"Custom manager file '{file_relative}' configured in {APP_FILE_APP_CONFIG} "
-                    f"does not exist at {file_abs_path}."
-                )
         return workdir
 
