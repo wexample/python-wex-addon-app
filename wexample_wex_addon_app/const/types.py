5e14a3fb8e6ac497738d81c77a9ded38ef294fa7
weeger <romain.weeger@gmail.com>
2025-08-22 10:47:58 +0200

    Modernize

--- wexample_wex_addon_app/const/types.py
+++ wexample_wex_addon_app/const/types.py
@@ -4,17 +4,17 @@ from wexample_helpers.const.types import StringKeysDict
 
 
 class AppConfig(TypedDict):
-    branch: Optional[str]
+    branch: str | None
     domain_main: str
     domain_tld: str
-    domains: List[str]
+    domains: list[str]
     domains_string: str
     env: StringKeysDict
     name: str
-    host: Dict[str, str]
-    password: Dict[str, str]
-    path: Dict[str, str]
+    host: dict[str, str]
+    password: dict[str, str]
+    path: dict[str, str]
     server: StringKeysDict
     service: StringKeysDict
     started: bool
-    user: Dict[str, str | int]
+    user: dict[str, str | int]
