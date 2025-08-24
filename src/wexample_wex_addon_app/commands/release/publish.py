3e7b86e47ec16bc5655978c6a7aa94cb64c7fba1
weeger <romain.weeger@gmail.com>
2025-08-24 17:39:56 +0200

    Rectify

--- src/wexample_wex_addon_app/commands/release/publish.py
+++ src/wexample_wex_addon_app/commands/release/publish.py
@@ -10,30 +10,29 @@ if TYPE_CHECKING:
 
 @command(description="Publish the Python package to PyPI.")
 def app__release__publish(
-        context: ExecutionContext,
+    context: ExecutionContext,
 ) -> None:
-    _python_app__release__publish(
-        context=context
-    )
+    _python_app__release__publish(context=context)
 
 
 # TODO Find a way to define that current app is a python package (services ?)
 def _python_app__release__publish(
-        context: ExecutionContext,
+    context: ExecutionContext,
 ) -> None:
     from wexample_helpers.helpers.shell import shell_run
+
     workdir = context.request.get_addon_manager().app_workdir
 
     # Map token to PyPI's token-based authentication if provided
-    username = '__token__'
-    password = workdir.get_env_parameter('PIPY_TOKEN')
+    username = "__token__"
+    password = workdir.get_env_parameter("PIPY_TOKEN")
 
     # Build the publish command, adding credentials only when given
-    publish_cmd = ['pdm', 'publish']
+    publish_cmd = ["pdm", "publish"]
     if username is not None:
-        publish_cmd += ['--username', username]
+        publish_cmd += ["--username", username]
     if password is not None:
-        publish_cmd += ['--password', password]
+        publish_cmd += ["--password", password]
 
     shell_run(
         publish_cmd,
