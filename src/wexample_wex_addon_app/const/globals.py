b192baeb6cee48ffb60909439a579cd6eb797157
weeger <romain.weeger@gmail.com>
2025-08-25 14:24:06 +0200

    Fix dependencies

--- src/wexample_wex_addon_app/const/globals.py
+++ src/wexample_wex_addon_app/const/globals.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-from wexample_wex_core.const.globals import CORE_COMMAND_NAME
-
-APP_DIR_APP_CONFIG: str = f".{CORE_COMMAND_NAME}"
 APP_ENV_DEV: str = "dev"
 APP_ENV_LOCAL: str = "local"
 APP_ENV_PROD: str = "prod"
