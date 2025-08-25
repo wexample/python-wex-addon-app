059a62f6568cc082791b90878642f7e9018b0f68
weeger <romain.weeger@gmail.com>
2025-08-25 09:00:08 +0200

    Harmonize versions

--- src/wexample_wex_addon_app/workdir/framework_package_workdir.py
+++ src/wexample_wex_addon_app/workdir/framework_package_workdir.py
@@ -1,7 +1,19 @@
 from __future__ import annotations
 
+from abc import abstractmethod
+
 from wexample_wex_core.workdir.project_workdir import ProjectWorkdir
 
 
 class FrameworkPackageWorkdir(ProjectWorkdir):
-    pass
+    @abstractmethod
+    def get_package_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_dependencies(self) -> list[str]:
+        pass
+
+    def ensure_dependency_declaration(self, searched_package: FrameworkPackageWorkdir) -> bool:
+        """Search if package is used in the current one, and update dependencies if not declared into."""
+        return True
--- src/wexample_wex_addon_app/workdir/framework_packages_suite_workdir.py
+++ src/wexample_wex_addon_app/workdir/framework_packages_suite_workdir.py
@@ -1,7 +1,32 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from wexample_wex_core.workdir.project_workdir import ProjectWorkdir
+
+if TYPE_CHECKING:
+    from wexample_wex_addon_app.workdir.framework_package_workdir import FrameworkPackageWorkdir
 
 
 class FrameworkPackageSuiteWorkdir(ProjectWorkdir):
-    pass
+    def get_local_packages_names(self) -> list[str]:
+        return [p.get_package_name() for p in self.get_packages()]
+
+    def build_dependencies(self) -> dict[str, list[str]]:
+        dependencies = {}
+        for package in self.get_packages():
+            dependencies[package.get_package_name()] = self.filter_local_packages(package.get_dependencies())
+
+        return dependencies
+
+    def get_packages(self) -> list[FrameworkPackageWorkdir]:
+        pip_dir = self.find_by_name(item_name='pip')
+        if pip_dir:
+            return pip_dir.get_children_list()
+        return []
+
+    def get_package(self, package_name: str) -> FrameworkPackageWorkdir | None:
+        for package in self.get_packages():
+            if package.get_package_name() == package_name:
+                return package
+        return None
