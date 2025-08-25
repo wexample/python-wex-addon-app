79d5ad37787a96e213d6135f516ea1b4f7ea0720
weeger <romain.weeger@gmail.com>
2025-08-25 14:41:28 +0200

    Updating versions

--- src/wexample_wex_addon_app/commands/suite/publish.py
+++ src/wexample_wex_addon_app/commands/suite/publish.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_wex_core.workdir.framework_packages_suite_workdir import FrameworkPackageSuiteWorkdir
 from wexample_wex_core.decorator.command import command
+from wexample_wex_core.workdir.framework_packages_suite_workdir import FrameworkPackageSuiteWorkdir
 
 if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
@@ -19,4 +19,10 @@ def app__suite__publish(
         context.io.warning(f'The current path is not a suite manager: {workdir.get_path()}')
         return
 
-    workdir.packages_harmonize_versions()
+    context.io.task('Checking internal dependencies...')
+    workdir.packages_validate_internal_dependencies_declarations()
+    context.io.success('Internal dependencies matches.')
+
+    context.io.task('Propagating versions across packages...')
+    workdir.packages_propagate_versions()
+    context.io.success('Versions updated.')
