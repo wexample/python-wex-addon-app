fb9501fff428b7ac8cd78844b58f53323b83b108
weeger <romain.weeger@gmail.com>
2025-08-26 12:23:58 +0200

    Rectify

--- src/wexample_wex_addon_app/commands/suite/publish.py
+++ src/wexample_wex_addon_app/commands/suite/publish.py
@@ -13,18 +13,20 @@ if TYPE_CHECKING:
 
 @command(description="Publish the Python package to PyPI.")
 def app__suite__publish(
-        context: ExecutionContext,
+    context: ExecutionContext,
 ) -> None:
     workdir = context.request.get_addon_manager().app_workdir
 
     if not isinstance(workdir, FrameworkPackageSuiteWorkdir):
-        context.io.warning(f'The current path is not a suite manager: {workdir.get_path()}')
+        context.io.warning(
+            f"The current path is not a suite manager: {workdir.get_path()}"
+        )
         return
 
-    context.io.task('Checking internal dependencies...')
+    context.io.task("Checking internal dependencies...")
     workdir.packages_validate_internal_dependencies_declarations()
-    context.io.success('Internal dependencies matches.')
+    context.io.success("Internal dependencies matches.")
 
-    context.io.task('Propagating versions across packages...')
+    context.io.task("Propagating versions across packages...")
     workdir.packages_propagate_versions()
-    context.io.success('Versions updated.')
+    context.io.success("Versions updated.")
