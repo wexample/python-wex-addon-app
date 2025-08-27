1115e5019b6c851a03a8f49187179272a1c89a4b
weeger <romain.weeger@gmail.com>
2025-08-27 11:16:09 +0200

    Rectify

--- src/wexample_wex_addon_app/commands/suite/publish.py
+++ src/wexample_wex_addon_app/commands/suite/publish.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_wex_core.decorator.command import command
+from wexample_wex_core.decorator.option import option
 from wexample_wex_core.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
@@ -11,6 +12,7 @@ if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
 
 
+@option(name="yes", type=bool, default=False, is_flag=True)
 @command(description="Publish the Python package to PyPI.")
 def app__suite__publish(
         context: ExecutionContext,
