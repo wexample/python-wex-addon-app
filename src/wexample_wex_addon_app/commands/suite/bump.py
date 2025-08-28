48b3283bf6214b9b8e771e032c746b4b0cd00cdc
weeger <romain.weeger@gmail.com>
2025-08-28 08:17:16 +0200

    Bump single

--- src/wexample_wex_addon_app/commands/suite/bump.py
+++ src/wexample_wex_addon_app/commands/suite/bump.py
@@ -3,17 +3,30 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_wex_core.decorator.command import command
+from wexample_wex_core.decorator.option import option
 
 if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
 
 
+@option(name="all", type=bool, default=False, is_flag=True)
+@option(name="package", type=str)
 @command(description="Bump version for every package of the suite.")
 def app__suite__bump(
-    context: ExecutionContext,
+        context: ExecutionContext,
+        all: bool | None = None,
+        package: str | None = None
 ) -> None:
     # Now we can initialize.
     workdir = context.request.get_addon_manager().app_workdir()
 
-    for package in workdir.get_packages():
+    if all is True:
+        for package in workdir.get_packages():
+            package.bump()
+    elif package is not None:
+        package = workdir.get_package(
+            package_name=package
+        )
         package.bump()
+    else:
+        context.io.warning('You should choose --all or --package [name] option')
