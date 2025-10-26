e2bc7a294c482f4a231d1d4ef23f19c3c4a9182e
weeger <romain.weeger@gmail.com>
2025-10-26 13:47:06 +0100

    Middlewares

--- src/wexample_wex_addon_app/middleware/app_middleware.py
+++ src/wexample_wex_addon_app/middleware/app_middleware.py
@@ -8,20 +8,13 @@ from wexample_wex_core.middleware.abstract_middleware import AbstractMiddleware
 
 @base_class
 class AppMiddleware(AbstractMiddleware):
-    def __init__(self, **kwargs) -> None:
-        # Define the app_path option
-        if "options" not in kwargs:
-            kwargs["options"] = []
-        
-        kwargs["options"].append(self._get_default_option())
-        
-        super().__init__(**kwargs)
-    
-    def _get_default_option(self) -> dict[str, Any]:
-        """Get the default app_path option definition."""
-        return {
+
+    def _get_middleware_options(self) -> list[dict[str, Any]]:
+        """Get the default file option definition."""
+        return [{
             "name": "app_path",
             "type": str,
             "required": False,
             "description": "Path to the app directory (defaults to current directory)",
-        }
+        }]
+
