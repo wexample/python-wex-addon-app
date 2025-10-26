05485dbbd21b87c2d82af94f376decc6ad9105dc
weeger <romain.weeger@gmail.com>
2025-10-26 16:01:00 +0100

    Refactor

--- src/wexample_wex_addon_app/commands/info/show.py
+++ src/wexample_wex_addon_app/commands/info/show.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_wex_addon_app.middleware.app_middleware import AppMiddleware
+from wexample_wex_addon_app.workdir.mixin.app_workdir_mixin import AppWorkdirMixin
 from wexample_wex_core.const.globals import COMMAND_TYPE_ADDON
 from wexample_wex_core.decorator.command import command
 from wexample_wex_core.decorator.middleware import middleware
@@ -15,17 +16,15 @@ if TYPE_CHECKING:
 @command(type=COMMAND_TYPE_ADDON)
 def app__info__show(
         context: ExecutionContext,
-        app_path: str
+        app_workdir: AppWorkdirMixin
 ) -> None:
     from wexample_helpers.helpers.cli import cli_make_clickable_path
 
-    workdir = context.request.get_addon_manager().app_workdir(path=app_path)
-
     context.io.properties(
         {
-            "Name": workdir.get_item_name(),
-            "Version": workdir.get_project_version(),
-            "Path": cli_make_clickable_path(path=workdir.get_path()),
+            "Name": app_workdir.get_item_name(),
+            "Version": app_workdir.get_project_version(),
+            "Path": cli_make_clickable_path(path=app_workdir.get_path()),
         },
         title="Application info",
     )
