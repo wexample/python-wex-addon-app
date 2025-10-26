43a508b263b9c3d514e66b66f62ee84b9dd60ed7
weeger <romain.weeger@gmail.com>
2025-10-26 09:44:23 +0100

    Middlewares

--- src/wexample_wex_addon_app/middleware/each_suite_package_middleware.py
+++ src/wexample_wex_addon_app/middleware/each_suite_package_middleware.py
@@ -2,11 +2,27 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from wexample_helpers.decorator.base_class import base_class
 from wexample_wex_core.middleware.abstract_middleware import AbstractMiddleware
 
 if TYPE_CHECKING:
-    pass
+    from wexample_helpers.const.types import Kwargs
+
+    from wexample_wex_core.common.command_method_wrapper import CommandMethodWrapper
+    from wexample_wex_core.common.command_request import CommandRequest
+    from wexample_wex_core.context.execution_context import ExecutionContext
 
 
+@base_class
 class EachSuitePackageMiddleware(AbstractMiddleware):
-    pass
+    def build_execution_contexts(
+        self,
+        command_wrapper: CommandMethodWrapper,
+        request: CommandRequest,
+        function_kwargs: Kwargs,
+    ) -> list[ExecutionContext]:
+        return super().build_execution_contexts(
+            command_wrapper=command_wrapper,
+            request=request,
+            function_kwargs=function_kwargs,
+        )
