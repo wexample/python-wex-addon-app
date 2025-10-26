2a5f8135135ede0161017567006ea1b25d18695a
weeger <romain.weeger@gmail.com>
2025-10-26 10:01:26 +0100

    Middlewares

--- src/wexample_wex_addon_app/commands/info/show.py
+++ src/wexample_wex_addon_app/commands/info/show.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from wexample_wex_addon_app.middleware.app_middleware import AppMiddleware
 from wexample_wex_core.const.globals import COMMAND_TYPE_ADDON
 from wexample_wex_core.decorator.command import command
+from wexample_wex_core.decorator.middleware import middleware
 
 if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
 
 
+@middleware(middleware=AppMiddleware)
 @command(type=COMMAND_TYPE_ADDON)
 def app__info__show(
     context: ExecutionContext,
