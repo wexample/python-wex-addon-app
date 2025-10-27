6d1a4baa2c6a81cb38b38440e80d9679d560186f
weeger <romain.weeger@gmail.com>
2025-10-27 19:11:40 +0100

    Rect

--- src/wexample_wex_addon_app/middleware/suite_or_each_package_middleware.py
+++ src/wexample_wex_addon_app/middleware/suite_or_each_package_middleware.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
 from wexample_helpers.decorator.base_class import base_class
+
 from wexample_wex_addon_app.middleware.package_suite_middleware import (
     PackageSuiteMiddleware,
 )
--- src/wexample_wex_addon_app/workdir/basic_app_workdir.py
+++ src/wexample_wex_addon_app/workdir/basic_app_workdir.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_helpers.decorator.base_class import base_class
-from wexample_wex_addon_app.workdir.mixin.app_workdir_mixin import AppWorkdirMixin
 from wexample_wex_core.workdir.workdir import Workdir
+
+from wexample_wex_addon_app.workdir.mixin.app_workdir_mixin import AppWorkdirMixin
 
 if TYPE_CHECKING:
     from wexample_filestate.result.file_state_result import FileStateResult
