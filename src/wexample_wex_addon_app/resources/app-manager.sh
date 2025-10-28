d2d5e74137c370a11f0cef65873174069fcf9548
weeger <romain.weeger@gmail.com>
2025-10-28 09:18:06 +0100

    Setup

--- src/wexample_wex_addon_app/resources/app-manager.sh
+++ src/wexample_wex_addon_app/resources/app-manager.sh
@@ -5,4 +5,8 @@ APP_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 AM_DIR="$APP_ROOT/.wex/python/app_manager"
 WEX_TASK_ID="$(date '+%Y%m%d-%H%M%S-%N')-$$"
 
-exec "$AM_DIR/.venv/bin/python" "$AM_DIR/__main__.py" "${WEX_TASK_ID}" "${@}"
+if [ "${1:-}" = "setup" ]; then
+  echo "setup"
+else
+  exec "$AM_DIR/.venv/bin/python" "$AM_DIR/__main__.py" "${WEX_TASK_ID}" "${@}"
+fi
