5705b561d19f60a63c297c25fc14136d8766047f
weeger <romain.weeger@gmail.com>
2025-10-28 17:12:03 +0100

    Various

--- src/wexample_wex_addon_app/commands/info/show.py
+++ src/wexample_wex_addon_app/commands/info/show.py
@@ -18,21 +18,21 @@ def app__info__show(
     context: ExecutionContext,
     app_workdir: BasicAppWorkdir,
 ) -> None:
-    from wexample_app.const.env import ENV_NAME_LOCAL
     from wexample_helpers.helpers.cli import cli_make_clickable_path
 
+    env = app_workdir.get_app_env()
     context.io.properties(
         {
             "Name": app_workdir.get_item_name(),
             "Version": app_workdir.get_project_version(),
             "Path": cli_make_clickable_path(path=app_workdir.get_path()),
-            "Environment": app_workdir.get_app_env(),
+            "Environment": env,
         },
         title="Application info",
     )
 
     # Show local libraries if configured
-    local_libraries = app_workdir.get_local_libraries_paths(env=ENV_NAME_LOCAL)
+    local_libraries = app_workdir.get_local_libraries_paths(env=env)
     if local_libraries:
         context.io.log("Libraries:", indentation=1)
         for lib_path in local_libraries:
