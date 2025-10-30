7c9d0c9a3529b7616e1b7cd5c2cc0e1df36b0d4a
weeger <romain.weeger@gmail.com>
2025-10-30 14:16:25 +0100

    Info

--- src/wexample_wex_addon_app/commands/info/show.py
+++ src/wexample_wex_addon_app/commands/info/show.py
@@ -10,30 +10,31 @@ from wexample_wex_core.decorator.middleware import middleware
 
 if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
+    from wexample_app.response.dict_response import DictResponse
 
 
 @middleware(middleware=AppMiddleware)
 @command(type=COMMAND_TYPE_ADDON)
 def app__info__show(
-    context: ExecutionContext,
-    app_workdir: BasicAppWorkdir,
-):
+        context: ExecutionContext,
+        app_workdir: BasicAppWorkdir,
+) -> DictResponse:
     from wexample_app.response.dict_response import DictResponse
 
     env = app_workdir.get_app_env()
-    
+
     data = {
-        "Name": app_workdir.get_item_name(),
-        "Version": app_workdir.get_project_version(),
-        "Path": str(app_workdir.get_path()),
-        "Environment": env,
+        "name": app_workdir.get_item_name(),
+        "version": app_workdir.get_project_version(),
+        "path": str(app_workdir.get_path()),
+        "environment": env,
     }
-    
+
     # Show local libraries if configured
     local_libraries = app_workdir.get_local_libraries_paths()
     if local_libraries:
-        data["Libraries"] = [library_config.get_str() for library_config in local_libraries]
-    
+        data["libraries"] = [library_config.get_str() for library_config in local_libraries]
+
     return DictResponse(
         kernel=context.kernel,
         content=data
