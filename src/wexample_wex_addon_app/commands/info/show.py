34bcbbcd657d5cc60f92be50c57555db934c694b
weeger <romain.weeger@gmail.com>
2025-11-03 20:29:22 +0100

    App info

--- src/wexample_wex_addon_app/commands/info/show.py
+++ src/wexample_wex_addon_app/commands/info/show.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_wex_addon_app.middleware.app_middleware import AppMiddleware
+from wexample_wex_addon_app.response.app_info_response import AppInfoResponse
 from wexample_wex_addon_app.workdir.basic_app_workdir import BasicAppWorkdir
 from wexample_wex_core.const.globals import COMMAND_TYPE_ADDON
 from wexample_wex_core.decorator.command import command
@@ -16,25 +17,10 @@ if TYPE_CHECKING:
 @middleware(middleware=AppMiddleware)
 @command(type=COMMAND_TYPE_ADDON)
 def app__info__show(
-    context: ExecutionContext,
-    app_workdir: BasicAppWorkdir,
+        context: ExecutionContext,
+        app_workdir: BasicAppWorkdir,
 ) -> DictResponse:
-    from wexample_app.response.dict_response import DictResponse
-
-    env = app_workdir.get_app_env()
-
-    data = {
-        "name": app_workdir.get_item_name(),
-        "version": app_workdir.get_project_version(),
-        "path": str(app_workdir.get_path()),
-        "environment": env,
-    }
-
-    # Show local libraries if configured
-    local_libraries = app_workdir.get_local_libraries_paths()
-    if local_libraries:
-        for library_config in local_libraries:
-            if library_config.is_str():
-                data["libraries"] = library_config.get_str()
-
-    return DictResponse(kernel=context.kernel, content=data)
+    return AppInfoResponse(
+        app_workdir=app_workdir,
+        kernel=context.kernel
+    )
---
+++ src/wexample_wex_addon_app/response/app_info_response.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wexample_app.const.env import ENV_COLORS
+from wexample_app.response.abstract_response import AbstractResponse
+from wexample_helpers.classes.field import public_field
+from wexample_helpers.decorator.base_class import base_class
+from wexample_prompt.responses.abstract_prompt_response import AbstractPromptResponse
+from wexample_prompt.responses.data.multiple_prompt_response import MultiplePromptResponse
+from wexample_prompt.responses.interactive.progress_prompt_response import ProgressPromptResponse
+from wexample_wex_addon_app.workdir.basic_app_workdir import BasicAppWorkdir
+
+if TYPE_CHECKING:
+    pass
+
+
+@base_class
+class AppInfoResponse(AbstractResponse):
+    app_workdir: BasicAppWorkdir = public_field(
+        description="The application the information is about"
+    )
+
+    def _get_formatted_prompt_response(self) -> AbstractPromptResponse:
+        from wexample_prompt.responses.data.properties_prompt_response import (
+            PropertiesPromptResponse,
+        )
+
+        env = self.app_workdir.get_app_env()
+
+        data = {}
+        # Show local libraries if configured
+        local_libraries = self.app_workdir.get_local_libraries_paths()
+        if local_libraries:
+            for library_config in local_libraries:
+                if library_config.is_str():
+                    data["libraries"] = library_config.get_str()
+
+        return MultiplePromptResponse.create_multiple(
+            responses=[
+                PropertiesPromptResponse(
+                    properties={
+                        "name": f"@color:blue{{{self.app_workdir.get_item_name()}}}",
+                        "version": self.app_workdir.get_project_version(),
+                        "path": str(self.app_workdir.get_path()),
+                        "environment": f"@color:{ENV_COLORS[env]}{{{env}}}",
+                    },
+                ),
+                ProgressPromptResponse(
+                    total=100,
+                    current=10,
+                    label="Test"
+                )
+            ]
+        )
