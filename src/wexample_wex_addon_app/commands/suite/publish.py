6df59ebec9a1860816d365af22553e70178e34b2
weeger <romain.weeger@gmail.com>
2025-11-12 15:59:08 +0100

    Option

--- src/wexample_wex_addon_app/commands/suite/publish.py
+++ src/wexample_wex_addon_app/commands/suite/publish.py
@@ -2,17 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_wex_core.const.globals import COMMAND_TYPE_ADDON
-from wexample_wex_core.decorator.command import command
-from wexample_wex_core.decorator.middleware import middleware
-from wexample_wex_core.decorator.option import option
-
 from wexample_wex_addon_app.middleware.package_suite_middleware import (
     PackageSuiteMiddleware,
 )
 from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
+from wexample_wex_core.const.globals import COMMAND_TYPE_ADDON
+from wexample_wex_core.decorator.command import command
+from wexample_wex_core.decorator.middleware import middleware
+from wexample_wex_core.decorator.option import option
 
 if TYPE_CHECKING:
     from wexample_wex_core.context.execution_context import ExecutionContext
@@ -20,20 +19,24 @@ if TYPE_CHECKING:
 
 @option(name="yes", type=bool, default=False, is_flag=True)
 @option(name="force", type=bool, default=False, is_flag=True)
+@option(name="ignore_dependencies", type=bool, default=False, is_flag=True)
 @middleware(middleware=PackageSuiteMiddleware)
 @command(
     type=COMMAND_TYPE_ADDON,
     description="Publish package to PyPI. Use --all-packages to publish all packages in suite.",
 )
 def app__suite__publish(
-    context: ExecutionContext,
-    app_workdir: FrameworkPackageSuiteWorkdir,
-    yes: bool = False,
-    force: bool = False,
+        context: ExecutionContext,
+        app_workdir: FrameworkPackageSuiteWorkdir,
+        yes: bool = False,
+        force: bool = False,
+        ignore_dependencies: bool = False
 ) -> None:
     from wexample_prompt.enums.terminal_color import TerminalColor
 
-    app_workdir.packages_validate_internal_dependencies_declarations()
+    if not ignore_dependencies:
+        app_workdir.packages_validate_internal_dependencies_declarations()
+
     packages = app_workdir.get_ordered_packages()
 
     context.io.log("Starting deployment...")
