0143ecf6a2beb6c7ec598a3892426c4e75f9ee12
weeger <romain.weeger@gmail.com>
2025-11-22 10:33:52 +0100

    Readme detection

--- src/wexample_wex_addon_app/workdir/mixin/with_readme_workdir_mixin.py
+++ src/wexample_wex_addon_app/workdir/mixin/with_readme_workdir_mixin.py
@@ -33,7 +33,8 @@ class WithReadmeWorkdirMixin(BaseClass):
         return config
 
     def has_readme(self) -> bool:
-        return self.find_by_name(APP_PATH_README) is not None
+        readme = self.find_by_name(APP_PATH_README)
+        return readme.get_path().exists() if readme else None
 
     def _get_readme_content(self) -> ReadmeContentConfigValue:
         from wexample_filestate.config_value.readme_content_config_value import (
