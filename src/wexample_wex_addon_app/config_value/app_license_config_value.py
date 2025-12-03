37fcbfb5100efb8c6920b92f3f485c2f4e8bb5e4
weeger <romain.weeger@gmail.com>
2025-12-03 10:59:04 +0100

    Add license

--- src/wexample_wex_addon_app/config_value/app_license_config_value.py
+++ src/wexample_wex_addon_app/config_value/app_license_config_value.py
@@ -3,16 +3,17 @@ from __future__ import annotations
 from wexample_filestate.config_value.aggregated_templates_config_value import AggregatedTemplatesConfigValue
 from wexample_helpers.classes.field import public_field
 from wexample_helpers.decorator.base_class import base_class
+from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 @base_class
 class AppLicenseConfigValue(AggregatedTemplatesConfigValue):
-    workdir = public_field(description="The application workdir")
+    workdir: CodeBaseWorkdir = public_field(description="The application workdir")
 
     def get_templates(self) -> list[str] | None:
-        from datetime import datetime; print(datetime.now().year)
+        from datetime import datetime
 
         return [
             'MIT License',
-            f'Copyright (c) {datetime.now().year}'
+            f'Copyright (c) {datetime.now().year} {self.workdir.get_vendor_name()}'
         ]
