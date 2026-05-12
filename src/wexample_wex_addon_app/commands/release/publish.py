06b42755457588dc82da9486d5e5faeb09f46eb0
weeger <romain.weeger@gmail.com>
2026-05-12 11:05:45 +0200

    Git rectification

--- src/wexample_wex_addon_app/commands/release/publish.py
+++ src/wexample_wex_addon_app/commands/release/publish.py
@@ -8,6 +8,7 @@ from wexample_wex_core.decorator.command import command
 from wexample_wex_core.decorator.middleware import middleware
 from wexample_wex_core.decorator.option import option
 
+from wexample_wex_addon_app.decorator.require_app_config import require_app_config
 from wexample_wex_addon_app.middleware.app_middleware import AppMiddleware
 from wexample_wex_addon_app.workdir.managed_workdir import ManagedWorkdir
 
@@ -17,6 +18,14 @@ if TYPE_CHECKING:
 
 @option(name="force", type=bool, default=False, is_flag=True)
 @as_sudo()
+@require_app_config(
+    path="git.publication_strategy",
+    type=str,
+    values=["main_push", "branch_merge"],
+    description="Publication strategy",
+    ask_question="Which publication strategy should be used for this app?",
+    on_missing="ask",
+)
 @middleware(middleware=AppMiddleware)
 @command(
     type=COMMAND_TYPE_ADDON,
