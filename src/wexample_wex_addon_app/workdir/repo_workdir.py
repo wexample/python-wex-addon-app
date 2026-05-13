54e9b49475ae2c864121d8f8310d669d93362923
weeger <romain.weeger@gmail.com>
2026-05-13 13:25:52 +0200

    Optional command

--- src/wexample_wex_addon_app/workdir/repo_workdir.py
+++ src/wexample_wex_addon_app/workdir/repo_workdir.py
@@ -314,7 +314,7 @@ class RepoWorkdir(ManagedWorkdir):
             )
             self.manager_run_command(command=app__version__propagate)
             sub_progress.advance(step=1, label=f"Building {self.get_project_name()}")
-            self._run_build_if_present()
+            self.manager_run_from_path(cmd=[".release/build", "--ignore-missing-command"])
             sub_progress.advance(step=1, label=f"Publishing {self.get_project_name()}")
             self._do_publish(force=force)
 
@@ -398,13 +398,6 @@ class RepoWorkdir(ManagedWorkdir):
 
     def _post_publish(self) -> None:
         pass
-
-    def _run_build_if_present(self) -> None:
-        from wexample_wex_addon_app.app_addon_manager import AppAddonManager
-
-        AppAddonManager.from_kernel(self.parent_io_handler).run_app_command(
-            ".release/build", self, silent=True
-        )
 
     def _publish(self, force: bool = False) -> None:
         pass
